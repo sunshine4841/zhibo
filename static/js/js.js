df4e28ecaf26750d776d548877639b5e0ee1e4f8
sunshine4841 <1582541665@qq.com>
2025-05-24 10:17:08 +0800

    Add files via upload

---
+++ static/js/js.js
@@ -0,0 +1,31 @@
+﻿$(function() {
+  const host = window.location.origin;
+  // const host = 'https://zhibo.duanshipin.com/';
+  // const api = host == 'https://zhibo.duanshipin.com' ? 'https://digital-live.ren.duanshipin.com' : 'https://digital-live.dev.ren.duanshipin.com';
+  const api = 'https://digital-live.ren.duanshipin.com';
+  console.log(host);
+  const hostList = ['https://zhibo.duanshipin.com', 'https://zhibotest.duanshipin.com']
+  if (!hostList.includes(host)) {
+    $.ajax({
+      url: `${api}/api/website`,
+      type: 'POST',
+      data: { host },
+      success: function(res) {
+        const { data } = res
+        $('title').html(data.title)
+        $('.bg-custom .btn').eq(0).attr('href', data.cloud)
+		$('.bg-custom .btn').eq(1).attr('href', data.manage)
+        $('.bg-custom .btn').eq(2).attr('href', data.plug)
+        $('link[rel="icon"]').attr('href', data.icon)
+        $('.bg-main').attr('src', data.bg_image)
+        $('.bg').hide()
+        $('.bg-custom-wrap').show()
+      }
+    })
+  } else {
+    $('.bg').show()
+    $('.bg-custom-wrap').hide()
+    $('title').html('创意兔-AI智播')
+    $('link[rel="icon"]').attr('href', './img/logo-mini.png')
+  }
+})
---
+++ static/js/js1.js
@@ -0,0 +1,8 @@
+﻿$(function() {
+  
+    $('.bg').show()
+    $('.bg-custom-wrap').hide()
+    $('title').html('星云-AI智播')
+    $('link[rel="icon"]').attr('href', './static/picture/mini-logo.png')
+  }
+)
